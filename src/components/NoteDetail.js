ca196c61db296e0081034cebb5e4f081172caa6e
falcon <falcon_chen@qq.com>
2024-08-30 08:41:49 +0800

    remove delete action when in add mode

--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -223,11 +223,13 @@ function NoteDetail() {
           <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
         </svg>
         <span className="header-title">{mode === 'add' ? '添加笔记' : '编辑笔记'}</span>
-        <div className="header-actions">
-          <svg className="icon" viewBox="0 0 24 24" fill="#333" onClick={handleDelete}>
-            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
-          </svg>
-        </div>
+        {mode !== 'add' && (
+          <div className="header-actions">
+            <svg className="icon" viewBox="0 0 24 24" fill="#333" onClick={handleDelete}>
+              <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+            </svg>
+          </div>
+        )}
       </header>
       
       <div className="edit-container">
